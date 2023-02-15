f7fc3a3c3ab50e5a116fb398dcf3e557bf0e2f8a
Dean Gilewicz <gilewiczd@gmail.com>
2023-02-15 00:52:06 -0700

    [update-cors-allow-deployed-site] - add netlify prod url for cors

--- src/cors.ts
+++ src/cors.ts
@@ -7,8 +7,12 @@ export function isAllowedOrigin(requestOrigin: string | undefined): boolean {
 	// Specific ports for FE
 	const allowedLocalPorts = [3000];
 
+	// Specific deployed application url (Netlify)
+	const allowedProductionUrl = "https://taupe-daifuku-ff591d.netlify.app";
+
 	const allowed: (string | RegExp)[] = [
 		...allowedLocalPorts.map((localPort) => `http://localhost:${localPort}`),
+		allowedProductionUrl,
 	];
 
 	return allowed.some((allowedOrigin) => {
